217be6625aa5882e6099fe15f3941d49d357e4cd
phrubel <phrubel42@gmail.com>
2023-06-11 18:50:56 +0600

    reg controller done

--- controllers/userController.js
+++ controllers/userController.js
@@ -22,7 +22,21 @@ const loginController = async (req, res) => {
 };
 
 // reg controller
-const regController = () => {};
+const regController = async (req, res) => {
+  try {
+    const newUser = new userModel(req.body);
+    await newUser.save();
+    res.status(201).json({
+      success: true,
+      newUser,
+    });
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      error,
+    });
+  }
+};
 
 // exports
 module.exports = { loginController, regController };
